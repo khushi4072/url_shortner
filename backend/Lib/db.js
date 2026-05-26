ccf437b707a8a1daa5bb0e395a421b0e59603ed0
khushi4072 <khushi.singh89208@gmail.com>
2026-05-26 19:57:09 +0530

    fix

--- backend/Lib/db.js
+++ backend/Lib/db.js
@@ -1,12 +1,11 @@
-// import ki jagah require use karo
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'khushisingh',
-    host: 'localhost', // AWS par database setup hone ke baad isse change karna pad sakta hai
-    database: 'url_shortener',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: 5432,
 });
 
-// export default ki jagah module.exports use karo
 module.exports = pool;
